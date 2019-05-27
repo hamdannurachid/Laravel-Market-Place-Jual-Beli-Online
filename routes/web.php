384f063784df465ded5ccaa5cce2935e96aa8ca5
hamdannurachid <hamdannurachid6@gmail.com>
2019-05-27 15:38:13 +0700

    tampilan home pengguna
    tampilan beranda untuk pengguna saat akses halaman utama

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/home', 'PenggunaController@index')->name('home');
 
