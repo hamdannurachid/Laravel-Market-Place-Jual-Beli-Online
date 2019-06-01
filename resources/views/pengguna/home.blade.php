697df8a94c42063287dca87aa62bdaf8a9b36a10
hamdannurachid <hamdannurachid6@gmail.com>
2019-06-01 16:32:49 +0700

    redesign home user
    login dan tampilkan nama pengguna

--- resources/views/pengguna/home.blade.php
+++ resources/views/pengguna/home.blade.php
@@ -11,6 +11,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <link href="{{ asset('markito-web/css/bootstrap.css') }}" rel="stylesheet" type="text/css" media="all" />
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="{{ asset('markito-web/js/jquery.min.js') }}"></script>
+<script src="{{ asset('js/app.js') }}" defer></script>
 <!-- Custom Theme files -->
 <!--theme-style-->
 <link href="{{ asset('markito-web/css/style.css') }}" rel="stylesheet" type="text/css" media="all" />
@@ -97,196 +98,17 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		<div class="top-nav">
 			  <ul class="megamenu skyblue">
 				      <li class="active grid"><a  href="#">fashion</a>
-<!-- 					    <div class="megapanel">
-						<div class="row">
-							<div class="col1">
-								<div class="h_nav">
-									<ul>
-										<li><a href="products.html">Accessories</a></li>
-										<li><a href="products.html">Bags</a></li>
-										<li><a href="products.html">Caps & Hats</a></li>
-										<li><a href="products.html">Hoodies & Sweatshirts</a></li>
-										<li><a href="products.html">Jackets & Coats</a></li>
-										<li><a href="products.html">Jeans</a></li>
-										<li><a href="products.html">Jewellery</a></li>
-										<li><a href="products.html">Jumpers & Cardigans</a></li>
-										<li><a href="products.html">Leather Jackets</a></li>
-										<li><a href="products.html">Long Sleeve T-Shirts</a></li>
-										<li><a href="products.html">Loungewear</a></li>
-									</ul>
-								</div>
-							</div>
-							<div class="col1">
-								<div class="h_nav">
-									<ul>
-										<li><a href="products.html">Shirts</a></li>
-										<li><a href="products.html">Shoes, Boots & Trainers</a></li>
-										<li><a href="products.html">Shorts</a></li>
-										<li><a href="products.html">Suits & Blazers</a></li>
-										<li><a href="products.html">Sunglasses</a></li>
-										<li><a href="products.html">Sweatpants</a></li>
-										<li><a href="products.html">Swimwear</a></li>
-										<li><a href="products.html">Trousers & Chinos</a></li>
-										<li><a href="products.html">T-Shirts</a></li>
-										<li><a href="products.html">Underwear & Socks</a></li>
-										<li><a href="products.html">Vests</a></li>
-									</ul>
-								</div>
-							</div>
-							<div class="col1">
-								<div class="h_nav">
-									<h4>Popular Brands</h4>
-									<ul>
-										<li><a href="products.html">Levis</a></li>
-										<li><a href="products.html">Persol</a></li>
-										<li><a href="products.html">Nike</a></li>
-										<li><a href="products.html">Edwin</a></li>
-										<li><a href="products.html">New Balance</a></li>
-										<li><a href="products.html">Jack & Jones</a></li>
-										<li><a href="products.html">Paul Smith</a></li>
-										<li><a href="products.html">Ray-Ban</a></li>
-										<li><a href="products.html">Wood Wood</a></li>
-									</ul>
-								</div>
-							</div>
-						  </div>
-						</div> -->
+
 					</li>
 					<li><a   href="#">furniture & decor</a>
-					<!--  <div class="megapanel">
-						<div class="row">
-							<div class="col1">
-								<div class="h_nav">
-									<ul>
-										<li><a href="decor.html">Bed</a></li>
-										<li><a href="decor.html">Chest</a></li>
-										<li><a href="decor.html">Stool & Reider</a></li>
-										<li><a href="decor.html">Chair & Carpet</a></li>
-										<li><a href="decor.html">Curtain & Brix</a></li>
-										<li><a href="decor.html">Shelf</a></li>
-										<li><a href="decor.html">Desk</a></li>
-										<li><a href="decor.html">Sofa & Anson</a></li>
-										<li><a href="decor.html">Bench</a></li>
-										<li><a href="decor.html">Sink</a></li>
-										<li><a href="decor.html">Lamp</a></li>
-									</ul>
-								</div>
-							</div>
-							<div class="col1">
-								<div class="h_nav">
-									<ul>
-										<li><a href="decor.html">Armchair</a></li>
-										<li><a href="decor.html">Reider Bench</a></li>
-										<li><a href="decor.html">Carpet & Desk</a></li>
-										<li><a href="decor.html">Wardrobe & Lamp</a></li>
-										<li><a href="decor.html">Car seat</a></li>
-										<li><a href="decor.html">Lounger</a></li>
-										<li><a href="decor.html">Anson</a></li>
-										<li><a href="decor.html">Karlstad</a></li>
-										<li><a href="decor.html">Camilla & Wardrobe</a></li>
-										<li><a href="decor.html">Colton</a></li>
-										<li><a href="decor.html">Brix</a></li>
-									</ul>
-								</div>
-							</div>
-							<div class="col1">
-							<iframe src="https://player.vimeo.com/video/16878818"  webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-							</div>
-						  </div>
-						</div> --></li>
+</li>
 				    <li class="grid"><a  href="#">mobiles & tablets</a>
-					   <!-- <div class="megapanel">
-						<div class="row">
-							<div class="col1">
-								<div class="h_nav">
-									<ul>
-										<li><a href="mobile.html">Lenovo Tablets</a></li>
-										<li><a href="mobile.html">Motorola</a></li>
-										<li><a href="mobile.html">Samsung </a></li>
-										<li><a href="mobile.html">Htc Tab</a></li>
-										<li><a href="mobile.html">Dell & Compaq</a></li>
-										<li><a href="mobile.html">Asus Tablets</a></li>
-										<li><a href="mobile.html">Microsoft</a></li>
-										<li><a href="mobile.html">Moto E & Moto G</a></li>
-										<li><a href="mobile.html">Intex</a></li>
-										<li><a href="mobile.html">Hauwei Lumia</a></li>
-										<li><a href="mobile.html">Loungewear</a></li>
-									</ul>
-								</div>
-							</div>
-							<div class="col1">
-								<div class="h_nav">
-									<ul>
-										<li><a href="mobile.html">Asus Zenfone 2</a></li>
-										<li><a href="mobile.html">Nikon & Sony</a></li>
-										<li><a href="mobile.html">Shorts</a></li>
-										<li><a href="mobile.html">Olymplus</a></li>
-										<li><a href="mobile.html">Sunglasses</a></li>
-										<li><a href="mobile.html">Samsung Nx</a></li>
-										<li><a href="mobile.html">Printers & Monitors</a></li>
-										<li><a href="mobile.html">Routers</a></li>
-										<li><a href="mobile.html">Data Cards</a></li>
-										<li><a href="mobile.html">Mouse & Keyboard</a></li>
-										<li><a href="mobile.html">Ink Cartridges</a></li>
-									</ul>
-								</div>
-							</div>
-							<div class="col1">
-								<div class="col1">
-								<iframe src="https://player.vimeo.com/video/8118831"  webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-							</div>
-							</div>
-						  </div>
-						</div> -->
+
 			    </li>
 						 <li class="grid"><a  href="health.html">health & beauty</a>
 
 			    </li>
 				<li><a  href="#">tvs, gaming & cameras</a>
-				  <!-- <div class="megapanel">
-						<div class="row">
-							<div class="col1">
-								<div class="h_nav">
-									<ul>
-										<li><a href="mobile.html">Lenovo Tablets</a></li>
-										<li><a href="mobile.html">Motorola</a></li>
-										<li><a href="mobile.html">Samsung </a></li>
-										<li><a href="mobile.html">Htc Tab</a></li>
-										<li><a href="mobile.html">Dell & Compaq</a></li>
-										<li><a href="mobile.html">Asus Tablets</a></li>
-										<li><a href="mobile.html">Microsoft</a></li>
-										<li><a href="mobile.html">Moto E & Moto G</a></li>
-										<li><a href="mobile.html">Intex</a></li>
-										<li><a href="mobile.html">Hauwei Lumia</a></li>
-										<li><a href="mobile.html">Loungewear</a></li>
-									</ul>
-								</div>
-							</div>
-							<div class="col1">
-								<div class="h_nav">
-									<ul>
-										<li><a href="mobile.html">Asus Zenfone 2</a></li>
-										<li><a href="mobile.html">Nikon & Sony</a></li>
-										<li><a href="mobile.html">Shorts</a></li>
-										<li><a href="mobile.html">Olymplus</a></li>
-										<li><a href="mobile.html">Sunglasses</a></li>
-										<li><a href="mobile.html">Samsung Nx</a></li>
-										<li><a href="mobile.html">Printers & Monitors</a></li>
-										<li><a href="mobile.html">Routers</a></li>
-										<li><a href="mobile.html">Data Cards</a></li>
-										<li><a href="mobile.html">Mouse & Keyboard</a></li>
-										<li><a href="mobile.html">Ink Cartridges</a></li>
-									</ul>
-								</div>
-							</div>
-							<div class="col1">
-								<div class="col1">
-								<iframe src="https://player.vimeo.com/video/8118831"  webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-							</div>
-							</div>
-						  </div>
-						</div></li> -->
-
 
 			  </ul>
 				</div>
@@ -305,20 +127,55 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
                     <div class="cart box_1">
 						{{-- <button type="button" class="btn btn-primary">Primary</button> --}}
 
-                        {{-- <div class="total">
-                            <a href="#" class="btn btn-info active" role="button" aria-pressed="true">Login</a></div>
 
 
-                            <div class="total">
-                                <a href="#" class="btn btn-danger active" role="button" aria-pressed="true">Register</a></div>
 
 
-                            <div class="clearfix"> </div> --}}
+
 
                             @if (Route::has('login'))
                             <div class="total">
+
                                 @auth
-                                    <a href="{{ url('/home') }}">Home</a>
+                                    {{-- <a href="{{ url('/') }}">Home</a> --}}
+                                    <nav class="main-nav float-right d-none d-lg-block">
+                                            <ul>
+                                                    <li class="nav-item dropdown">
+                                                            <a href="">
+                                                                    <h3>
+                                                                        <img src="{{ asset ('markito-web/images/cart.png') }}" alt=""/></h3>
+                                                                    </a>
+                                                    </li>
+
+                                    <li class="nav-item dropdown dropdown-menu-right">
+
+                                        <a id="navbarDropdown" class="nav-link dropdown-toggle btn btn-danger active " href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+
+                                            {{ Auth::user()->name }} <span class="far fa-address-book"></span>
+                                        </a>
+
+                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                            <a class="" href="">
+                                                {{ __('Profil Saya') }}
+                                            </a>
+                                            <br>
+                                            <a class="" href="{{ route('logout') }}"
+                                               onclick="event.preventDefault();
+                                                             document.getElementById('logout-form').submit();">
+                                                {{ __('Logout') }}
+                                            </a>
+
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                @csrf
+                                            </form>
+                                        </div>
+
+                                    </li>
+
+                                </ul>
+
+                            </nav>
+
                                 @else
                                     <a href="{{ route('login') }}" class="btn btn-info active" role="button" aria-pressed="true">Login</a>
 
@@ -330,6 +187,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
                         @endif
 
                     </div>
+
 
 
 
