7f9273a6c96c9c50e9e85cee5df7c035d67350b2
faisalmuhamadgupron <50608288+faisalmuhamadgupron@users.noreply.github.com>
2019-06-25 13:06:20 +0700

    templet daftar product

--- resources/views/pengguna/list_product.blade.php
+++ resources/views/pengguna/list_product.blade.php
@@ -1,5 +1,446 @@
-@extends('pengguna.home')
+@extends('pengguna.index')
 
 @section('content')
-<h1>INI UNTUK KOLOM TAMPILAN PRODUK</h1>
+
+<div class="container">
+		<div class="product">
+		<h2 class="new">NEW ARRIVALS</h2>
+		<div class="pink">
+			<div class="col-md-3 box-in-at">
+			<div class=" grid_box portfolio-wrapper">		
+							 <a href="single.html" > <img src= {{asset('markito-web/images/fa.jpg')}}" class="img-responsive" alt="">
+							 	<div class="zoom-icon">
+							 		
+									<ul class="in-by">
+										<li><h5>sizes:</h5></li>                     
+										<li><span>S</span></li>
+										<li><span>XS</span></li>
+										<li><span>M</span></li>
+										<li><span> L</span></li>
+										<li><span>XL</span></li>
+										<li><span> XXL</span></li>
+									</ul>
+					
+					
+						<ul class="in-by-color">
+							<li><h5>colors:</h5></li>                   
+							<li><span > </span></li>
+							<li><span class="color"> </span></li>
+							<li><span class="color1"> </span></li>
+							<li><span class="color2"> </span></li>
+							<li><span class="color3"> </span></li>
+							
+						</ul>
+					
+						</div> </a> 	
+		           </div>
+				<!---->
+						<div class="grid_1 simpleCart_shelfItem">
+							<a href="#" class="cup item_add"><span class=" item_price" >123 $ <i> </i> </span></a>					
+						</div>
+					<!---->
+				</div>
+
+			
+				<div class="col-md-3 box-in-at">
+			<div class=" grid_box portfolio-wrapper">		
+							 <a href="single.html" > <img {{asset('markito-web/images/fa1.jpg')}}" class="img-responsive" alt="">
+							 	<div class="zoom-icon">
+							 		
+									<ul class="in-by">
+										<li><h5>sizes:</h5></li>                     
+										<li><span>S</span></li>
+										<li><span>XS</span></li>
+										<li><span>M</span></li>
+										<li><span> L</span></li>
+										<li><span>XL</span></li>
+										<li><span> XXL</span></li>
+									</ul>
+					
+					
+						<ul class="in-by-color">
+							<li><h5>colors:</h5></li>                   
+							<li><span > </span></li>
+							<li><span class="color"> </span></li>
+							<li><span class="color1"> </span></li>
+							<li><span class="color2"> </span></li>
+							<li><span class="color3"> </span></li>
+							
+						</ul>
+					
+						</div> </a> 	
+		           </div>
+				<!---->
+						<div class="grid_1 simpleCart_shelfItem">
+							<a href="#" class="cup item_add"><span class=" item_price" >123 $ <i> </i> </span></a>					
+						</div>
+					<!---->
+				</div>
+
+			
+				<div class="col-md-3 box-in-at">
+			<div class=" grid_box portfolio-wrapper">		
+							 <a href="single.html" > <img {{asset('markito-web/images/fa2.jpg')}}" class="img-responsive" alt="">
+							 	<div class="zoom-icon">
+							 		
+									<ul class="in-by">
+										<li><h5>sizes:</h5></li>                     
+										<li><span>S</span></li>
+										<li><span>XS</span></li>
+										<li><span>M</span></li>
+										<li><span> L</span></li>
+										<li><span>XL</span></li>
+										<li><span> XXL</span></li>
+									</ul>
+					
+					
+						<ul class="in-by-color">
+							<li><h5>colors:</h5></li>                   
+							<li><span > </span></li>
+							<li><span class="color"> </span></li>
+							<li><span class="color1"> </span></li>
+							<li><span class="color2"> </span></li>
+							<li><span class="color3"> </span></li>
+							
+						</ul>
+					
+						</div> </a> 	
+		           </div>
+				<!---->
+						<div class="grid_1 simpleCart_shelfItem">
+							<a href="#" class="cup item_add"><span class=" item_price" >123 $ <i> </i> </span></a>					
+						</div>
+					<!---->
+				</div>
+
+			
+				<div class="col-md-3 box-in-at">
+			<div class=" grid_box portfolio-wrapper">		
+							 <a href="single.html" > <img {{asset('markito-web/images/fa3.jpg')}}" class="img-responsive" alt="">
+							 	<div class="zoom-icon">
+							 		
+									<ul class="in-by">
+										<li><h5>sizes:</h5></li>                     
+										<li><span>S</span></li>
+										<li><span>XS</span></li>
+										<li><span>M</span></li>
+										<li><span> L</span></li>
+										<li><span>XL</span></li>
+										<li><span> XXL</span></li>
+									</ul>
+					
+					
+						<ul class="in-by-color">
+							<li><h5>colors:</h5></li>                   
+							<li><span > </span></li>
+							<li><span class="color"> </span></li>
+							<li><span class="color1"> </span></li>
+							<li><span class="color2"> </span></li>
+							<li><span class="color3"> </span></li>
+							
+						</ul>
+					
+						</div> </a> 	
+		           </div>
+				<!---->
+						<div class="grid_1 simpleCart_shelfItem">
+							<a href="#" class="cup item_add"><span class=" item_price" >123 $ <i> </i> </span></a>					
+						</div>
+					<!---->
+				</div>
+				<div class="clearfix"> </div>
+		</div>
+		<div class="pink">
+			<div class="col-md-3 box-in-at">
+			<div class=" grid_box portfolio-wrapper">		
+							 <a href="single.html" > <img {{asset('markito-web/images/fa4.jpg')}}" class="img-responsive" alt="">
+							 	<div class="zoom-icon">
+							 		
+									<ul class="in-by">
+										<li><h5>sizes:</h5></li>                     
+										<li><span>S</span></li>
+										<li><span>XS</span></li>
+										<li><span>M</span></li>
+										<li><span> L</span></li>
+										<li><span>XL</span></li>
+										<li><span> XXL</span></li>
+									</ul>
+					
+					
+						<ul class="in-by-color">
+							<li><h5>colors:</h5></li>                   
+							<li><span > </span></li>
+							<li><span class="color"> </span></li>
+							<li><span class="color1"> </span></li>
+							<li><span class="color2"> </span></li>
+							<li><span class="color3"> </span></li>
+							
+						</ul>
+					
+						</div> </a> 	
+		           </div>
+				<!---->
+						<div class="grid_1 simpleCart_shelfItem">
+							<a href="#" class="cup item_add"><span class=" item_price" >123 $ <i> </i> </span></a>					
+						</div>
+					<!---->
+				</div>
+
+			
+				<div class="col-md-3 box-in-at">
+			<div class=" grid_box portfolio-wrapper">		
+							 <a href="single.html" > <img {{asset('markito-web/images/fa5.jpg')}}" class="img-responsive" alt="">
+							 	<div class="zoom-icon">
+							 		
+									<ul class="in-by">
+										<li><h5>sizes:</h5></li>                     
+										<li><span>S</span></li>
+										<li><span>XS</span></li>
+										<li><span>M</span></li>
+										<li><span> L</span></li>
+										<li><span>XL</span></li>
+										<li><span> XXL</span></li>
+									</ul>
+					
+					
+						<ul class="in-by-color">
+							<li><h5>colors:</h5></li>                   
+							<li><span > </span></li>
+							<li><span class="color"> </span></li>
+							<li><span class="color1"> </span></li>
+							<li><span class="color2"> </span></li>
+							<li><span class="color3"> </span></li>
+							
+						</ul>
+					
+						</div> </a> 	
+		           </div>
+				<!---->
+						<div class="grid_1 simpleCart_shelfItem">
+							<a href="#" class="cup item_add"><span class=" item_price" >123 $ <i> </i> </span></a>					
+						</div>
+					<!---->
+				</div>
+
+			
+				<div class="col-md-3 box-in-at">
+			<div class=" grid_box portfolio-wrapper">		
+							 <a href="single.html" > <img {{asset('markito-web/images/fa6.jpg')}}" class="img-responsive" alt="">
+							 	<div class="zoom-icon">
+							 		
+									<ul class="in-by">
+										<li><h5>sizes:</h5></li>                     
+										<li><span>S</span></li>
+										<li><span>XS</span></li>
+										<li><span>M</span></li>
+										<li><span> L</span></li>
+										<li><span>XL</span></li>
+										<li><span> XXL</span></li>
+									</ul>
+					
+					
+						<ul class="in-by-color">
+							<li><h5>colors:</h5></li>                   
+							<li><span > </span></li>
+							<li><span class="color"> </span></li>
+							<li><span class="color1"> </span></li>
+							<li><span class="color2"> </span></li>
+							<li><span class="color3"> </span></li>
+							
+						</ul>
+					
+						</div> </a> 	
+		           </div>
+				<!---->
+						<div class="grid_1 simpleCart_shelfItem">
+							<a href="#" class="cup item_add"><span class=" item_price" >123 $ <i> </i> </span></a>					
+						</div>
+					<!---->
+				</div>
+
+			
+				<div class="col-md-3 box-in-at">
+			<div class=" grid_box portfolio-wrapper">		
+							 <a href="single.html" > <img {{asset('markito-web/images/fa7.jpg')}}" class="img-responsive" alt="">
+							 	<div class="zoom-icon">
+							 		
+									<ul class="in-by">
+										<li><h5>sizes:</h5></li>                     
+										<li><span>S</span></li>
+										<li><span>XS</span></li>
+										<li><span>M</span></li>
+										<li><span> L</span></li>
+										<li><span>XL</span></li>
+										<li><span> XXL</span></li>
+									</ul>
+					
+					
+						<ul class="in-by-color">
+							<li><h5>colors:</h5></li>                   
+							<li><span > </span></li>
+							<li><span class="color"> </span></li>
+							<li><span class="color1"> </span></li>
+							<li><span class="color2"> </span></li>
+							<li><span class="color3"> </span></li>
+							
+						</ul>
+					
+						</div> </a> 	
+		           </div>
+				<!---->
+						<div class="grid_1 simpleCart_shelfItem">
+							<a href="#" class="cup item_add"><span class=" item_price" >123 $ <i> </i> </span></a>					
+						</div>
+					<!---->
+				</div>
+				<div class="clearfix"> </div>
+		</div>
+		<div class="pink">
+			<div class="col-md-3 box-in-at">
+			<div class=" grid_box portfolio-wrapper">		
+							 <a href="single.html" > <img {{asset('markito-web/images/fa8.jpg')}}" class="img-responsive" alt="">
+							 	<div class="zoom-icon">
+							 		
+									<ul class="in-by">
+										<li><h5>sizes:</h5></li>                     
+										<li><span>S</span></li>
+										<li><span>XS</span></li>
+										<li><span>M</span></li>
+										<li><span> L</span></li>
+										<li><span>XL</span></li>
+										<li><span> XXL</span></li>
+									</ul>
+					
+					
+						<ul class="in-by-color">
+							<li><h5>colors:</h5></li>                   
+							<li><span > </span></li>
+							<li><span class="color"> </span></li>
+							<li><span class="color1"> </span></li>
+							<li><span class="color2"> </span></li>
+							<li><span class="color3"> </span></li>
+							
+						</ul>
+					
+						</div> </a> 	
+		           </div>
+				<!---->
+						<div class="grid_1 simpleCart_shelfItem">
+							<a href="#" class="cup item_add"><span class=" item_price" >123 $ <i> </i> </span></a>					
+						</div>
+					<!---->
+				</div>
+
+			
+				<div class="col-md-3 box-in-at">
+			<div class=" grid_box portfolio-wrapper">		
+							 <a href="single.html" > <img {{asset('markito-web/images/fa.jpg')}}" class="img-responsive" alt="">
+							 	<div class="zoom-icon">
+							 		
+									<ul class="in-by">
+										<li><h5>sizes:</h5></li>                     
+										<li><span>S</span></li>
+										<li><span>XS</span></li>
+										<li><span>M</span></li>
+										<li><span> L</span></li>
+										<li><span>XL</span></li>
+										<li><span> XXL</span></li>
+									</ul>
+					
+					
+						<ul class="in-by-color">
+							<li><h5>colors:</h5></li>                   
+							<li><span > </span></li>
+							<li><span class="color"> </span></li>
+							<li><span class="color1"> </span></li>
+							<li><span class="color2"> </span></li>
+							<li><span class="color3"> </span></li>
+							
+						</ul>
+					
+						</div> </a> 	
+		           </div>
+				<!---->
+						<div class="grid_1 simpleCart_shelfItem">
+							<a href="#" class="cup item_add"><span class=" item_price" >123 $ <i> </i> </span></a>					
+						</div>
+					<!---->
+				</div>
+
+			
+				<div class="col-md-3 box-in-at">
+			<div class=" grid_box portfolio-wrapper">		
+							 <a href="single.html" > <img {{asset('markito-web/images/fa3.jpg')}}" class="img-responsive" alt="">
+							 	<div class="zoom-icon">
+							 		
+									<ul class="in-by">
+										<li><h5>sizes:</h5></li>                     
+										<li><span>S</span></li>
+										<li><span>XS</span></li>
+										<li><span>M</span></li>
+										<li><span> L</span></li>
+										<li><span>XL</span></li>
+										<li><span> XXL</span></li>
+									</ul>
+					
+					
+						<ul class="in-by-color">
+							<li><h5>colors:</h5></li>                   
+							<li><span > </span></li>
+							<li><span class="color"> </span></li>
+							<li><span class="color1"> </span></li>
+							<li><span class="color2"> </span></li>
+							<li><span class="color3"> </span></li>
+							
+						</ul>
+					
+						</div> </a> 	
+		           </div>
+				<!---->
+						<div class="grid_1 simpleCart_shelfItem">
+							<a href="#" class="cup item_add"><span class=" item_price" >123 $ <i> </i> </span></a>					
+						</div>
+					<!---->
+				</div>
+
+			
+				<div class="col-md-3 box-in-at">
+			<div class=" grid_box portfolio-wrapper">		
+							 <a href="single.html" > <img {{asset('markito-web/images/fa6.jpg')}}" class="img-responsive" alt="">
+							 	<div class="zoom-icon">
+							 		
+									<ul class="in-by">
+										<li><h5>sizes:</h5></li>                     
+										<li><span>S</span></li>
+										<li><span>XS</span></li>
+										<li><span>M</span></li>
+										<li><span> L</span></li>
+										<li><span>XL</span></li>
+										<li><span> XXL</span></li>
+									</ul>
+					
+					
+						<ul class="in-by-color">
+							<li><h5>colors:</h5></li>                   
+							<li><span > </span></li>
+							<li><span class="color"> </span></li>
+							<li><span class="color1"> </span></li>
+							<li><span class="color2"> </span></li>
+							<li><span class="color3"> </span></li>
+							
+						</ul>
+					
+						</div> </a> 	
+		           </div>
+				<!---->
+						<div class="grid_1 simpleCart_shelfItem">
+							<a href="#" class="cup item_add"><span class=" item_price" >123 $ <i> </i> </span></a>					
+						</div>
+					<!---->
+				</div>
+				<div class="clearfix"> </div>
+		</div>
+		</div>
+		<!---->	
+	</div>
 @endsection
